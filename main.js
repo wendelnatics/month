ffc93b56bb3a99c9d2990dde0b06fd3aefdaf7e3
Kiori <69706925+wendelnatics@users.noreply.github.com>
2022-08-18 12:51:15 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert('ready kana ba?')
+alert('para kiligin ka haha')
+alert('Iloveyou😚💦')
+alert('oh bat ngayon kinikilig kana agad Hahaha')
+alert('ganda mo last nato hahaha')
+alert('Iloveyou 😚💦')
+$(document).ready(function() {
+ setTimeout(function() {
+  $("#proceed").show();
+ }, 5000);
+});
+
+
